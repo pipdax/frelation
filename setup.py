0b92ccf6c0596fa0de7860962abaa80fbd9227b6
pipdax <pipdax@126.com>
2018-08-18 23:31:49 +0800

    添加setup.py文件，实现一键安装

---
+++ setup.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+
+from setuptools import setup
+
+setup(
+        name='frelation',
+        version='0.0.1',
+        description='在机器学习中，构造的特征多且乱，这个库用来展示已经构造的特征关系，方便查漏补缺',
+        author='pipdax',
+        author_email='pipdax@126.com',
+        url='https://github.com/pipdax/frelation',
+        packages=['frelation'],
+        install_requires=[    # 依赖列表
+        'pyecharts>=0.5.8',
+    ]
+)
